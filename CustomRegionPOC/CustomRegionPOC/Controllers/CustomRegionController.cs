c2ae11be4aa97d088f0001a39e253237119b87b9
10PEARLS\tahir.hassan <tahir.hassan@tenpearls.com>
2018-05-10 11:14:46 +0500

    Initial Commit

---
+++ CustomRegionPOC/CustomRegionPOC/Controllers/CustomRegionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomRegionPOC.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CustomRegion")]
+    public class CustomRegionController : Controller
+    {
+        
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
